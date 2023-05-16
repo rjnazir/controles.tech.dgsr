912a41b5ef6fa577844cdab42881f99e5bf7992a
Raharison Jaona Nazir <59433723+rjnazir@users.noreply.github.com>
2023-05-16 07:41:10 +0300

    Modification du 15/05/2023

--- src/Form/CtAnomalieType.php
+++ src/Form/CtAnomalieType.php
@@ -5,11 +5,11 @@ namespace App\Form;
 use App\Entity\CtAnomalie;
 use App\Entity\CtAnomalieType as CtAnomalieTypes;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CtAnomalieType extends AbstractType
 {
