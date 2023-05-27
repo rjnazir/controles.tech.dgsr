ac0ea662438d7947d632bdd577ab10426874e4ae
Raharison Jaona Nazir <59433723+rjnazir@users.noreply.github.com>
2023-05-27 12:40:26 +0300

    Modification du 27/05/2023

--- src/Controller/CtExpressionBesoinController.php
+++ src/Controller/CtExpressionBesoinController.php
@@ -126,42 +126,56 @@ class CtExpressionBesoinController extends AbstractController
 
         $ct_centre = $this->getUser()->getCtCentre()->getCtrNom();
         $rgts_ctre = $ctCentreRepository->transformCenter($ct_centre, '69');
-        $ctrLibelle = $rgts_ctre[2];
+        $ctrFonction = $rgts_ctre[0];
         $ctrNom = $rgts_ctre[1];
+        $ctrLibelle = $rgts_ctre[2];
         $ctrNumero = $rgts_ctre[4];
 
         $contenue = $ctExpressionBesoinRepository->findBy(['ctCentre'=>$ctCentre, 'edbDateEdit'=>$edbDateEdit]);
         
-        $pdf = new TCPDF();
-
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
         $pdf->AddPage('P');
         $pdf->setTitle('Expression de besoin');
-        $pdf->setPrintHeader(false);
-        $pdf->setPrintFooter(false);
 
-        $pdf->SetFont('helvetica','',7);
+		$pdf->setPrintFooter(false);
+		$pdf->setPrintHeader(false);
+		$pdf->setFooterFont(Array('times', '', 10));
+		$pdf->SetHeaderMargin(10);
+		$pdf->SetFooterMargin(10);
+		$pdf->SetAutoPageBreak(true, 10);
+
+        $pdf->SetFont('times','',9);
 		$pdf->SetTextColor(0,0,0);
 		$pdf->SetFillColor(0,0,0,0);
 		$pdf->MultiCell(95,5,"DIRECTION GENERALE DE LA SECURITE ROUTIERE",0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,"REPOBLIKAN'I MADAGASIKARA",0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,"--------------------",0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
-		$pdf->SetFont('helvetica','I',7);
+		$pdf->SetFont('times','I',9);
 		$pdf->MultiCell(95,5,"Fitiavana - Tanindrazana - Fandrosoana",0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
-		$pdf->SetFont('helvetica','',7);
+		$pdf->SetFont('times','',9);
 		$pdf->MultiCell(95,5,"DIRECTION DES OPERATIONS ROUTIERES",0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,"--------------------",0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,"--------------------",0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,"" ,0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
+		$pdf->SetFont('times','B',9);
 		$pdf->MultiCell(95,5,$ctrLibelle.' '.$ctrNom,0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
+		$pdf->SetFont('times','',9);
 		$pdf->MultiCell(95,5,'A '.ucwords(strtolower($ctrNom)).', '.$ctCentreRepository->dateLetterFr(),0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,"--------------------",0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
 		$pdf->MultiCell(95,5,$ctrNumero,0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
+        $pdf->SetXY(52.5, 40);
+        $pdf->Image('sb-admin-2/images/logo_dgsr.jpg', '', '', 10, 10, '', '', '', true, 300, '', false, false, false, false, false, false);
 		$pdf->Ln(10);
+		$pdf->SetFont('times','',7);
+		$pdf->MultiCell(95,5,"LAHITOKANA NY AINA",0,'C',true,0,'', '', true, 0, false, true, 5, 'M');
+		$pdf->MultiCell(95,5,"",0,'C',true,1,'', '', true, 0, false, true, 5, 'M');
+		$pdf->Ln(8);
 
-		$pdf->SetFont('Helvetica','UB',12);
+
+		$pdf->SetFont('times','UB',14);
 		$pdf->SetTextColor(0,0,0);
 		$pdf->Cell(190,8,"EXPRESSION DES BESOINS",0,0,'C');
-		$pdf->Ln(10);
+		$pdf->Ln(15);
 
 		$entete = array();
 		$entete[0] = "N°";
@@ -171,30 +185,44 @@ class CtExpressionBesoinController extends AbstractController
 		$entete[4] = "OBSERVATIONS";
 		$pdf->SetTextColor(0,0,0);
 		//En-tête tableau
-		$pdf->SetFont('helvetica','B',7);
-		$w=array(10,90,30,30,30);
-		$pdf->SetFillColor(0,148,255);
+		$pdf->SetFont('times','B',12);
+		$w=array(10,75,34,34,37);
+		$pdf->SetFillColor(255,255,255);
 		for($i=0;$i<count($entete);$i++){
-			$pdf->Cell($w[$i],10,($entete[$i]),1,0,'C',0);
+            if($i< (count($w) - 1 )){
+                $pdf->MultiCell($w[$i], 12, $entete[$i], 1, 'C', 1, 0, '', '', true, 0, false, true, 12, 'M');
+            }else{
+                $pdf->MultiCell($w[$i], 12, $entete[$i], 1, 'C', 1, 1, '', '', true, 0, false, true, 12, 'M');
+            }
 		}
-		$pdf->Ln(10);
+        //Contenu du tablau
 		$pdf->SetTextColor(0,0,0);
-		$pdf->SetFont('helvetica','',7);
+		$pdf->SetFont('times','',12);
 		if(count($contenue) !=0){
             $i = 1;
 			foreach($contenue as $contenue) {
-				$pdf->Cell($w[0],8,$i,1,0,'C');
-				$pdf->Cell($w[1],8,$contenue->getCtImprimeTech()->getNomImprimeTech().' ('.$contenue->getCtImprimeTech()->getAbrevImprimeTech().')',1);
-				$pdf->Cell($w[2],8,"",1,0,'C');
-				$pdf->Cell($w[3],8,$contenue->getEdbQteDemander(),1,0,'C');
-				$pdf->Cell($w[4],8,"",1,0,'C');
-				$pdf->Ln(8);
+				$pdf->Cell($w[0],12,$i,1,0,'C');
+				$pdf->Cell($w[1],12,$contenue->getCtImprimeTech()->getNomImprimeTech().' ('.$contenue->getCtImprimeTech()->getAbrevImprimeTech().')',1);
+				$pdf->Cell($w[2],12,"",1,0,'C');
+				$pdf->Cell($w[3],12,$contenue->getEdbQteDemander(),1,0,'C');
+				$pdf->Cell($w[4],12,"",1,0,'C');
+				$pdf->Ln(12);
                 $i++;
 			}
 		}else{
-			$pdf->Cell(190,8,"NEANT",1,0,"C");
-			$pdf->Ln(4);
+			$pdf->Cell(190,12,"NEANT",1,0,"C");
+			$pdf->Ln(12);
 		}
+
+        //Signataire et avis de l'EDB
+		$pdf->SetTextColor(0, 0, 0);
+		$pdf->SetFont('times', '', 12);
+        $pdf->Cell(95, 12, '', 0, 0, 'C');
+        $pdf->Cell(95, 12, ucfirst(strtolower(str_replace('LE ', '', $ctrFonction))), 0, 1, 'C');
+        $pdf->Ln(40);
+		$pdf->SetFont('times', 'UB', 12);
+        $pdf->Cell(95, 12, 'Avis du responsable MAG.APPRO', 0, 0, 'C');
+        $pdf->Cell(95, 12, 'Décision AC', 0, 1, 'C');
 
         $pdf->Output('edb_'.date('YmdHis').'.pdf', 'I');
 
